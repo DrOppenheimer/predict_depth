5d7cc021ef2adc2973d3655abbec551e2a2e5ff9
Kevin Keegan <kevin.p.keegan@gmail.com>
2013-02-14 17:11:51 -0600

    added the scripts to the repo for the first time

---
+++ predict_depth.r
@@ -0,0 +1,91 @@
+predict_depth <- function(abundance_matrix, input_type = "object", file_out_prefix = "depth_prediction", genome_size=5000000, coverage=100, num_to_show=10, create_figure=TRUE){
+
+# Print usage if
+   if (nargs() == 0){print_usage()}
+
+# Check to see if abundance_matrix is a file - if so, load it - if not, assume it is an R object and copy it
+   if (input_type=="file"){
+     my_data.matrix <- data.matrix(read.table(abundance_matrix, row.names=1, check.names=FALSE, header=TRUE, sep="\t", comment.char="", quote=""))
+   }else{
+     # my_data.matrix <- my_data$count
+     my_data.matrix <- abundance_matrix
+   }
+   
+# get names
+  row_names <- dimnames(my_data.matrix)[1]
+  col_names <- dimnames(my_data.matrix)[2]
+
+# get index of exisiting order
+  my_data.index <- as.vector(order(my_data.matrix[,1], decreasing=TRUE))
+
+# create index sorted matrix # this preserves the row names, but not columns
+  sorted_matrix <- as.matrix(my_data.matrix[my_data.index,])
+  dimnames(sorted_matrix)[2] <- col_names
+  
+# create matrix to hold calcualted depths
+  my_coverage_matrix <- matrix("",dim(sorted_matrix)[1],2)
+  dimnames(my_coverage_matrix)[[1]] <- dimnames(sorted_matrix)[[1]] # label rows
+  dimnames(my_coverage_matrix)[[2]] <- c("16s abundance","WGS depth to assemble (bp)") # label columns
+  my_coverage_matrix[,1] <- sorted_matrix[,1] # place abundance in first column
+
+# calculate sequencing depth for each taxon
+  for (i in 1:dim(sorted_matrix)[1]){
+    my_coverage_matrix[i,2] <- round( ( genome_size * coverage ) / ( sorted_matrix[i,1] / sum(sorted_matrix[,1]) ) )
+  }
+
+# generate tab delimited output
+  file_out <- gsub(" ", "", paste(file_out_prefix, ".txt"))
+  write.table(my_coverage_matrix, file = "my_depth_prediction.txt", col.names=NA, row.names = TRUE, sep="\t", quote=FALSE)
+  
+# generate a figure 
+  if (create_figure == TRUE){
+    image_out <- gsub(" ", "", paste(file_out_prefix, ".pdf"))
+    pdf(file=image_out, width = 10, height = 5)
+    par(mar=c(15,5,1,5))
+    barplot( as.numeric(sorted_matrix[1:num_to_show,1]), las=2, axisnames=(1:10), names.arg = dimnames(my_coverage_matrix)[[1]][1:num_to_show], ylab="" ) # mar=c(1,1,1,50))
+    mtext("Taxon Abundance", side=2, line=4 )
+    par(new=TRUE)
+    plot((1:num_to_show), my_coverage_matrix[1:num_to_show,2], type="o", col="red", lwd=3, lty=1, xlab="", ylab="", xaxt="n", axes=F)
+    #axis(2, las=1)
+    #mtext("Taxon Abundance", side=2, line=1 )
+    axis(4, las=1, col="red")
+    mtext( paste("Predicted BPs of sequencing for (", coverage, ") x coverage"),side=4, line=4, adj=1.3, col = "red")
+    dev.off()
+  }
+  
+ }
+
+
+print_usage <- function() {
+  writeLines(
+             "  ------------------------------------------------------------------------------
+  predict_depth.r                     Kevin P. Keegan, kkeegan@anl.gov  Feb 2013
+  ------------------------------------------------------------------------------
+  DESCRIPTION:
+  Script to predict amount of WGS sequencing necessary to achieve a certain level of
+  coverage based on relative organism abundance determined from 16s data. 
+
+  USAGE:
+  predict_depth <- function(abundance_matrix, input_type = c(\"file\", \"object\"), file_out_prefix= \"depth_prediction\", genome_size=5000000, coverage=100, num_to_show=10, create_figure=TRUE)
+
+
+  NOTES:
+
+  abundance_matrix : can be a file or R matrix - specify file or it will treat like R matrix
+  an output tab delimited text is always created, a pdf is output is optional.
+  Calculation is performed on all taxa - but only shows as many as specified by
+  num_to_show.
+
+  Two most commone ways to use this script would be like this for an R object:
+       predict_depth(my_data.matrix)
+  or like this for a tab delimited file as input
+       predict_depth(\"test_data.txt\", input_type=\"file\")
+
+  In either case, input is a two column matrix, first with taxa names, second
+  with their abundances.
+  ------------------------------------------------------------------------------"
+             )
+  stop("you did not enter the correct args -- see above")
+}
+
+
---
+++ predict_depth.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+#written 2-14-13
+#if [ $# -eq 0 || $# -gt 3 ] # usage for no or more than 3 arguments                                                                                 
+if [ $# -eq 0 -o $# -gt 4 ]
+then
+    echo "DESCRIPTION: Shell wrapper for predict_depth.r"
+    echo 
+    echo "USAGE: >predict_depth.sh <file_in> <file_out_prefix> <produce_pdf> <show>"
+    echo   
+    echo "     <file_in>:         NO DEFAULT: string, name of the input file" 
+    echo "                        (two column, tab delimited, annotations then abundances" 
+    echo "                        first row, first column (annotation header) is blank, first" 
+    echo "                        row, scecond column can have a text header)"
+    echo
+    echo "     <file_out_prefix>: Default = \"depth_prediction\"  :string, prefix for output file(s)" 
+    echo "                        *.txt (& *.pdf if produce_fig = 1)"
+    echo
+    echo "     <produce_pdf>:     Default = TRUE:   TRUE|FALSE, produce a pdf visulization of the output"
+    echo
+    echo "     <show>:            Default = 10: Integer, number of taxa to show in pdf"
+    echo
+    echo "EXAMPLES: predict_depth.sh test_data.txt "
+    echo "          predict_depth.sh test_data.txt \"depth_prediction\" TRUE 20"
+    echo
+    echo "Annoying: to change argument 2, you have to supply 1 and 2 etc."
+    echo "There are additional arguments in the function file (predict_depth.r)"
+    echo
+    exit 1                                                                                          # exit the script
+fi
+
+time_stamp=`date +%m-%d-%y_%X:%N`;  # create the time stamp month-day-year_hour:min:sec:nanosec
+
+# grab arguments from prompt
+file_in=$1
+file_out_prefix=$2
+produce_pdf=$3
+show=$4
+
+# set default values
+: ${file_out_prefix:="depth_prediction"}
+: ${produce_pdf:=1}
+: ${show:=10}
+
+echo "# shell generated script to run predict_depth.r" > shell.predict_depth.r.$time_stamp.r
+
+echo "source(\"~/bin/predict_depth.r\")" >> shell.predict_depth.r.$time_stamp.r       
+
+echo "predict_depth(abundance_matrix=\"$file_in\", input_type=\"file\", file_out_prefix = \"$file_out_prefix\", create_figure=$produce_pdf, num_to_show=$show)" >>  shell.predict_depth.r.$time_stamp.r
+
+R --vanilla --slave <  shell.predict_depth.r.$time_stamp.r
+rm  shell.predict_depth.r.$time_stamp.r
